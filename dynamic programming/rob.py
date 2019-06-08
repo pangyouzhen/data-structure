67f8a80b54b8e396fe6dd4bbf35cf9281336ab98
pangyouzhen <pangyouzhen@live.com>
2019-06-08 18:25:35 +0800

    fixed rob

--- dynamic programming/rob.py
+++ dynamic programming/rob.py
@@ -1,41 +1,50 @@
 # 实际问题转化为数学递归问题
 # 现在假设现在是最大值，增加新的元素应该怎么得到最大值
 # TODO
-def rob_n(n, ls):
-    if n == 0:
-        res = ls[0]
-    elif n == 1:
-        res = ls[1]
-    else:
-        print(n)
-        res = max(rob_n(n - 2, ls) + ls[n], rob_n(n - 1, ls))
-        # 4 element n=3 index from 0
-        # ls[1] + ls[3] | ls[2]
-        #
-        print("-------------")
-    return res
+# def rob_n(n, ls):
+#     if n == 0:
+#         res = ls[0]
+#     elif n == 1:
+#         res = ls[1]
+#     else:
+#         print(n)
+#         res = max(rob_n(n - 2, ls) + ls[n], rob_n(n - 1, ls))
+#         # 4 element n=3 index from 0
+#         # ls[1] + ls[3] | ls[2]
+#         #
+#         print("-------------")
+#     return res
+#
+#
+# def rob(ls):
+#     n = len(ls) - 1
+#     return rob_n(n, ls)
 
 
-def rob(ls):
-    n = len(ls) - 1
-    return rob_n(n, ls)
+
+# 动态规划的算法最好画表格，画树，表格有一般有两行，一个是index一个是需要求得值，一个简单的技巧使用memo  memo = [None] * (len(nums) + 1)
 
 
-# 大问题转化为小问题，如何将大问题与小问题关联
-# class Solution:
-#     def rob(self, nums):
-#         t = len(nums)
-#         a = 0
-#         while a < t:
-#             a = a + 1
-#         # ls_1 = [nums[i] for i in range(0, t, 2)]
-#         # ls_2 = [nums[i] for i in range(1, t, 2)]
-#         # print(ls_1, ls_2)
-#         # return max(sum(ls_1), sum(ls_2))
-#         pass
+
+class Solution:
+    def rob(self, nums):
+        if len(nums) == 0:
+            return 0
+        memo = [None] * (len(nums) + 1)
+        return self.rob_memo(nums, memo)
+
+    def rob_memo(self, nums, memo):
+        memo[0] = 0
+        memo[1] = nums[0]
+        for i, v in enumerate(nums[:-1]):
+            memo[i + 2] = max(memo[i + 1], memo[i] + nums[i + 1])
+        return memo[-1]
 
 
 if __name__ == '__main__':
-    # assert rob([1, 2, 3, 1]) == [1, 3]
-    assert rob([2, 3, 8, 6, 7]) == 17
-    assert rob([2, 1, 1, 2]) == 4
+    res = Solution().rob
+    assert res([2, 3, 8, 6, 7]) == 17
+    assert res([2, 1, 1, 2]) == 4
+    assert res([1, 2, 3, 1]) == 4
+    assert res([2, 7, 9, 3, 1]) == 12
+    assert res([1, 2, 3, 1]) == 4
