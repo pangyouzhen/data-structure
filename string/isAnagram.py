522747025d896f4cb9e07416d99b0cc322439f20
pangyouzhen <pangyouzhen@live.com>
2019-06-08 18:46:11 +0800

    isAnagram

--- string/isAnagram.py
+++ string/isAnagram.py
@@ -1,10 +1,14 @@
 class Solution:
     def isAnagram(self, s, t):
-        pass
+        st = sorted(tuple(list(s)))
+        tt = sorted(tuple(list(t)))
+        if st == tt and len(s) == len(t):
+            return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
-    sol = Solution()
-    s1 = "anagram"
-    s2 = ""
-    res = sol.isAnagram()
+    res = Solution().isAnagram
+    assert res("anagram", "nagaram") == True
+    assert res("rat", "car") == False
