133ffef57bb4c83429239d7122d02d5bd2444cd4
pangyouzhen <pangyouzhen@live.com>
2019-06-08 19:07:08 +0800

    myAtoi

--- string/myAtoi.py
+++ string/myAtoi.py
@@ -3,53 +3,17 @@ import re
 
 class Solution:
     def myAtoi(self, strs):
-        res = []
-        a = 0
-        while (a < len(strs)):
-            if not strs[a].isdigit() and strs[a] != "+" and strs[a] != "-":
-                break
+        regex_res = re.match("^(\s)*([+,-]?\d+)", strs)
+        res = 0
+        if regex_res:
+            gr = int(regex_res.group())
+            if gr > 2 ** 31 - 1:
+                res = 2 ** 31 - 1
+            elif gr < - 2 ** 31:
+                res = -2 ** 31
             else:
-                if strs[a].isdigit() and (a + 1) < len(strs) and strs[a + 1].isdigit():
-                    res.append(str(strs[a]))
-                elif (strs[a] == "+" or strs[a] == "-") and (a + 1) < len(strs) and strs[a + 1].isdigit():
-                    res.append(str(strs[a]))
-                elif a + 1 == len(strs):
-                    res.append(str(strs[a]))
-                else:
-                    break
-            a = a + 1
-        if "+" in res and "-" in res:
-            return 0
-        else:
-            res = list(filter(lambda x: x != "+", res))
-            if len(res) == 0:
-                return 0
-            elif len(res) == 1 and "-" in res:
-                return 0
-            elif int(''.join(res)) > 2 ** 31 - 1:
-                return 2 ** 31 - 1
-            elif int(''.join(res)) < - 2 ** 31:
-                return -2 ** 31
-            else:
-                return int(''.join(res))
-        # strs = re.sub(" ", "", strs)
-
-
-# print(strs[0])
-# regex_res = re.search("-{0,1}\d+", strs)
-# if regex_res:
-#     if strs[0] == "-" or strs[0] == "+" or strs[0].isdigit():
-#         gr = int(regex_res.group())
-#         if gr > 2 ** 31 - 1:
-#             return 2 ** 31 - 1
-#         elif gr < - 2 ** 31:
-#             return -2 ** 31
-#         else:
-#             return gr
-#     else:
-#         return 0
-# else:
-#     return 0
+                res = gr
+        return res
 
 
 if __name__ == '__main__':
@@ -63,4 +27,4 @@ if __name__ == '__main__':
     assert sol.myAtoi("+1") == 1
     assert sol.myAtoi("+-2") == 0
     assert sol.myAtoi("-") == 0
-    assert sol.myAtoi("0-1") == 0
+    assert sol.myAtoi("   -42") == -42
