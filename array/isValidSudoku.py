cf2d199a686518d7d5485c445fae9e9763c79f75
pangyouzhen <pangyouzhen@live.com>
2019-06-09 14:31:16 +0800

    some unimportant commit

--- array/isValidSudoku.py
+++ array/isValidSudoku.py
@@ -1,52 +1,21 @@
 class Solution:
     def isValidSudoku(self, board):
         # rows
-        # rows = 0
-        # while (rows < 9):
-        #     res = []
-        #     for v in board[rows]:
-        #         if (v in res) and (v != "."):
-        #             return False
-        #         else:
-        #             res.append(v)
-        #     rows = rows + 1
-        #
-        # # columns
-        # column = 0
-        # while (column < 9):
-        #     res = []
-        #     # while (a < 9):
-        #     columns_ = list(map(lambda x: x[column], board))
-        #     for v in columns_:
-        #         if (v in res) and (v != "."):
-        #             return False
-        #         else:
-        #             res.append(v)
-        #     column = column + 1
-        # board[0][1],board[1][1],board[2][1]...
+        rows = list(map(self.is_not_has_duplicate, board))
 
-        #  grid
-        grid = 0
-        while (grid < 3):
-            res = []
-            v = board[grid * 3:grid * 3 + 3]
-            grid_columns = list(map(lambda x: x[grid * 3, grid * 3 + 3], [v]))
-            print(grid_columns)
-            for v in grid_columns:
-                if (v in res) and (v != "."):
-                    return False
-                else:
-                    res.append(v)
-            pass
-        grid = grid + 1
-
-        #
         return True
+
+    def is_not_has_duplicate(self, nums):
+        t = list(filter(lambda x: x != ".", nums))
+        res = True
+        if len(t) != len(tuple(t)):
+            res = False
+        return res
 
 
 if __name__ == '__main__':
     sol = Solution()
-    res = sol.isValidSudoku([
+    assert sol.isValidSudoku( [
         ["8", "3", ".", ".", "7", ".", ".", ".", "."],
         ["6", ".", ".", "1", "9", "5", ".", ".", "."],
         [".", "9", "8", ".", ".", ".", ".", "6", "."],
@@ -56,5 +25,6 @@ if __name__ == '__main__':
         [".", "6", ".", ".", ".", ".", "2", "8", "."],
         [".", ".", ".", "4", "1", "9", ".", ".", "5"],
         [".", ".", ".", ".", "8", ".", ".", "7", "9"]
-    ])
-    print(res)
+    ]) == False
+    assert sol.is_not_has_duplicate(["8", "3", ".", ".", "7", ".", ".", ".", "."], ) == True
+    print(sol.isValidSudoku(["8", "3", ".", ".", "7", ".", ".", ".", "."], ))
