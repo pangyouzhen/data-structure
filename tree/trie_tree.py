ce3a240b2ec95b85b164f18113ad4ab97b2c2ad2
pangyouzhen <pangyouzhen@live.com>
2019-06-24 20:24:15 +0800

    fixed trie tree

--- tree/trie_tree.py
+++ tree/trie_tree.py
@@ -1,26 +1,21 @@
-class TrieNode(object):
-    pass
-
-
-class Trie:
-    def __int__(self):
+class Trie(object):
+    def __init__(self):
         """
         Initialize your data structure here
         """
-        self.root = TrieNode()
+        self.root = {}
 
     def insert(self, word):
         """
         Insert a word into the trie
         :param word: str
         """
-        node = self.root
-        for chars in word:
-            child = node.data.get(chars)
-            if not child:
-                node.data[chars] = TrieNode()
-            node = node.data[chars]
-        node.is_word = True
+        p = self.root
+        for c in word:
+            if c not in p:
+                p[c] = {}
+            p = p[c]
+        p["#"] = True
 
     def search(self, word):
         """
@@ -28,12 +23,8 @@ class Trie:
         :param word:  str
         :rtype: bool
         """
-        node = self.root
-        for chars in word:
-            node = node.data.get(chars)
-            if not node:
-                return False
-        return node.is_word
+        node = self.find(word)
+        return node is not None and "#" in node
 
     def startsWith(self, prefix):
         """
@@ -41,3 +32,22 @@ class Trie:
         :param prefix: str
         :return :bool
         """
+        return self.find(prefix) is not None
+
+    def find(self, prefix):
+        p = self.root
+        for c in prefix:
+            if c not in p:
+                return None
+            p = p[c]
+        return p
+
+
+if __name__ == '__main__':
+    sol = Trie()
+    sol.insert("word")
+    sol.insert("world")
+    sol.insert("wow")
+    sol.insert("www")
+    sol.search("www")
+    sol.search("wwl")
