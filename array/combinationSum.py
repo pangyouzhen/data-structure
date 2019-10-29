ff9cb750246fd95b969b3dc5b108990bcec91a4c
pangyouzhen <pangyouzhen@live.com>
2019-10-29 21:30:30 +0800

    itertools.product

---
+++ array/combinationSum.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        pass
+
+
--- string/letterCombinations.py
+++ string/letterCombinations.py
@@ -1,21 +1,28 @@
 from typing import List
+import itertools
+from functools import reduce
 
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        letter_dict = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
-        }
-        pass
+        if digits:
+            letter_dict = {
+                "2": "abc",
+                "3": "def",
+                "4": "ghi",
+                "5": "jkl",
+                "6": "mno",
+                "7": "pqrs",
+                "8": "tuv",
+                "9": "wxyz",
+            }
+            digits_ls = list(digits)
+            letter_ls = list(map(lambda x: list(letter_dict["{}".format(x)]), digits_ls))
+            product = lambda x, y: [''.join(list(y)) for y in itertools.product(x, y)]
+            res = reduce(product, letter_ls)
+            return res
 
 
 if __name__ == '__main__':
     sol = Solution()
-    sol.letterCombinations("23")
+    print(sol.letterCombinations("23"))
