350e346fce85e0158b6b1db05a51c2a0d9b785f6
pangyouzhen <pangyouzhen@live.com>
2020-03-14 13:44:44 +0800

    add 90

---
+++ backtracking/90 subsetsWithDup.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        # backtracking
+        def dfs(lst, nums, pos):
+            print(f'lst is {lst},nums is {nums}, pos is {pos}')
+            res1 = lst[:]
+            if res1 not in result:
+                result.append(res1)
+            for i in range(pos, len(nums)):
+                lst.append(nums[i])
+                dfs(lst, nums, i + 1)
+                lst.pop()
+
+        dfs([], nums, 0)
+        return result
+
+
+if __name__ == '__main__':
+    subsets = [1, 2, 2]
+    sol = Solution()
+    print(sol.subsetsWithDup(subsets))
