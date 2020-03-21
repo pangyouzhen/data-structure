1399d1a2659e87a0c93e991ab0c0869073453c9e
pangyouzhen <pangyouzhen@live.com>
2020-03-21 09:50:22 +0800

    add backtrack 77

---
+++ backtracking/77 combine.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        all_ans = []
+        one_ans = []
+
+        def dfs(all_ans, one_ans, array):
+            if len(one_ans) == k:
+                all_ans.append(one_ans[:])
+                return
+            for i in range(len(array)):
+                one_ans.append(array[i])
+                dfs(all_ans, one_ans, array[i + 1:])
+                one_ans.pop()
+
+        dfs(all_ans, one_ans, list(range(1, n + 1)))
+        return all_ans
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.combine(4, 2))
