8d6034e46ba969ed95b9767b39a422fbddde3d60
pangyouzhen <pangyouzhen@live.com>
2020-04-11 13:16:04 +0800

    add 701

---
+++ BST/230  kthSmallest.py
@@ -0,0 +1,9 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        pass
