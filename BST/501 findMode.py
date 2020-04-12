2692e5625d72c9ff1e51282577ab3dbfb2eef93c
pangyouzhen <pangyouzhen@live.com>
2020-04-12 15:29:36 +0800

    add 501

---
+++ BST/501 findMode.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+from typing import List
+from collections import Counter
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def findMode(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+
+        def inorder(root, a):
+            if root:
+                inorder(root.left, a)
+                a.append(root.val)
+                inorder(root.right, a)
+
+        a = []
+        inorder(root, a)
+        temp = Counter(a).most_common()
+        freq = temp[0][1]
+        result = [i[0] for i in temp if i[1] == freq]
+        return result
+
+
+if __name__ == '__main__':
+    tree = TreeNode(1)
+    tree.right = TreeNode(2)
+    tree.right.left = TreeNode(2)
+    sol = Solution()
+    print(sol.findMode(tree))
