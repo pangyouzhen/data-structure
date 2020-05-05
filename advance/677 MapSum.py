3d7195e4a030c8490f93923504a0a9146c71f328
pangyouzhen <pangyouzhen@live.com>
2020-05-05 18:50:34 +0800

    add 677 720

---
+++ advance/677 MapSum.py
@@ -0,0 +1,25 @@
+import collections
+
+
+class MapSum:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = collections.defaultdict(list)
+
+    def insert(self, key: str, val: int) -> None:
+        self.root[key] = val
+
+    def sum(self, prefix: str) -> int:
+        res = 0
+        for word, val in self.root.items():
+            if word.startswith(prefix):
+                res += val
+        return res
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
