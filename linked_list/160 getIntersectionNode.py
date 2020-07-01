5643b120c8ce215c32d14c016a49a2c2729b7162
pangyouzhen <pangyouzhen@live.com>
2020-07-01 18:42:55 +0800

    add create listnodes

--- linked_list/160 getIntersectionNode.py
+++ linked_list/160 getIntersectionNode.py
@@ -1,17 +1,43 @@
 # Definition for singly-linked list.
+from typing import List
+
+
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
+def create_listnodes(nums: List[int]) -> ListNode:
+    node = ListNode(nums.pop(0))
+    #  链表不同的是 一定要记住首要位置，指针不要跟着链表移动,最后返回头部就好
+    head = node
+    for element in nums:
+        # node.next = Node(data=element)
+        node.next = ListNode(element)
+        node = node.next
+    return  head
+
+
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        pass
+        reverse_headA = self.reverseListNode(headA)
+        reverse_headB = self.reverseListNode(headB)
+        while reverse_headA.val == reverse_headB.val:
+            reverse_headA = reverse_headA.next
+            reverse_headB = reverse_headB.next
+        return self.reverseListNode(reverse_headA)
 
     def reverseListNode(self, head: ListNode) -> ListNode:
-        pass
+        if head.next is None:
+            return head
+        last = self.reverseListNode(head.next)
+        head.next.next = head
+        head.next = None
+        return last
+
 
 if __name__ == '__main__':
-    headA = ListNode(8)
-    headB = ListNode()
+    a = [1, 2, 3, 4, 5, 6]
+    b = create_listnodes(a)
+    print('finish')
