b8a667f2d1dc47b6888e5cf02eb2a69bd97e9958
pangyouzhen <pangyouzhen@live.com>
2020-07-28 15:47:46 +0800

    add .

--- dynamic programming/198 rob.py
+++ dynamic programming/198 rob.py
@@ -4,6 +4,10 @@ from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
         # 这种 写法算是dp了毕竟是从下面往上推导，没有使用递归的公式
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
         n = len(nums)
         memo = [-1] * n
         memo[0] = nums[0]
@@ -11,8 +15,7 @@ class Solution:
         for i in range(2, n):
             res = max(memo[i - 2] + nums[i], memo[i - 1])
             memo[i] = res
-        print(memo)
-        return memo[n-1]
+        return memo[n - 1]
 
 
 if __name__ == '__main__':
