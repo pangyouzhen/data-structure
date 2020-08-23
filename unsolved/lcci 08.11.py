6739da7757746f1d196b0b7995ee3f5c2b5ff4df
pangyouzhen <pangyouzhen@live.com>
2020-08-23 15:43:21 +0800

    add unsolved

--- unsolved/lcci 08.11.py
+++ unsolved/lcci 08.11.py
@@ -11,14 +11,19 @@ class Solution:
             return 0
         if memo[n] != None:
             return memo[n]
-        for i in [25, 15, 10, 5]:
+        for i in [25, 15, 10, 5, 1]:
             res = max(1, self.waysToChange_memo(n - i))
         memo[n] = res
-        return res
+        return
 
+    # memo(10) = f(5+5) = f(10) = memo(5) +2
+
+
+# memo(2) =
+# [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 4,]
 
 if __name__ == '__main__':
     n = 10
     sol = Solution()
-    print(sol.waysToChange(10))
+    # print(sol.waysToChange(10))
     print(sol.waysToChange(2))
