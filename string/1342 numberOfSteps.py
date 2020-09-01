b41f29276e3eb69cdb36d870380e097865cdce82
pangyouzhen <pangyouzhen@live.com>
2020-09-01 08:01:34 +0800

    fixed 1342

--- string/1342 numberOfSteps.py
+++ string/1342 numberOfSteps.py
@@ -1,20 +1,22 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
         global res
-        res = 0
-        res = self.numberOfSteps_memo(num, 0)
-        return res
+        res = {"res": 0}
+        #  这里不能用整数，整数不起作用的原因是因为有歧义的，
+        #  不能确定是否是 local变量是赋值的还是global变量，local变量的优先级高于global所以不能这样用
+        self.numberOfSteps_memo(num)
+        return res["res"]
 
-    def numberOfSteps_memo(self, num, t):
+    def numberOfSteps_memo(self, num):
         if num == 0:
-            return t
+            return
         if num % 2 == 0:
             num = num / 2
         else:
             num = num - 1
-        res = res +1
-        t = self.numberOfSteps_memo(num, t + 1)
-        return t
+        res["res"] += 1
+        self.numberOfSteps_memo(num)
+        return
 
 
 if __name__ == '__main__':
