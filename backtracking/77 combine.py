0d50ebbd537fd8c2e6253b2fee46b3e01e075bc9
pangyouzhen <pangyouzhen@live.com>
2020-09-05 15:56:48 +0800

    add combinations

--- backtracking/77 combine.py
+++ backtracking/77 combine.py
@@ -1,39 +1,28 @@
-from typing import List
-
 
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        all_ans = []
-        one_ans = []
 
-        def dfs(all_ans, one_ans, array):
-            if len(one_ans) == k:
-                all_ans.append(one_ans[:])
-                return
-            for i in range(len(array)):
-                one_ans.append(array[i])
-                dfs(all_ans, one_ans, array[i + 1:])
-                one_ans.pop()
+    def combine(self, n: int, k: int):
+        global res
+        res = []
+        if n <= 0 or k <= 0 or k > n:
+            return res
+        c = []
+        self.combine_memo(n, k, 1, c)
+        return res
 
-        dfs(all_ans, one_ans, list(range(1, n + 1)))
-        return all_ans
+    def combine_memo(self, n: int, k: int, start: int, c):
+        if len(c) == k:
+            #  这里为什么 append(c[:]) 是正确的，但是append(c)最后返回的结果是 [[],[],[],...]
+            #  这里 append c是append的c的指针，所有的都是指的同一个对象，最后回溯的时候c被清空了，所以所有的res的值就变成了[[],[],[]]
+            res.append(c[:])
+            # res.append(c)
+            return
+        for i in range(start, n + 1):
+            c.append(i)
+            self.combine_memo(n, k, i + 1, c)
+            c.pop()
+        return
 
-
-    def combine_(self, nums: List[int], k: int) -> List[List[int]]:
-        all_ans = []
-        one_ans = []
-
-        def dfs(all_ans, one_ans, array):
-            if len(one_ans) == k:
-                all_ans.append(one_ans[:])
-                return
-            for i in range(len(array)):
-                one_ans.append(array[i])
-                dfs(all_ans, one_ans, array[i + 1:])
-                one_ans.pop()
-
-        dfs(all_ans, one_ans, list(range(1, n + 1)))
-        return all_ans
 
 if __name__ == '__main__':
     sol = Solution()
