832abf10de433d15023d25e727b304b06f3228b9
pangyouzhen <pangyouzhen@live.com>
2020-09-13 19:08:32 +0800

    fixed 64

--- base/dp/64 minPathSum.py
+++ base/dp/64 minPathSum.py
@@ -4,27 +4,26 @@ from pprint import pprint
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-
-        m, n = len(grid), len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
         dp = [[0] * n for _ in range(m)]
-        dp[0][0] = grid[0][0]
-        # 关键是前面这两个的判定, 这是针对第一行和第一列进行判定的
-        for i in range(1, m):
-            dp[i][0] = dp[i - 1][0] + grid[i][0]
-        for j in range(1, n):
-            dp[0][j] = dp[0][j - 1] + grid[0][j]
-        pprint(dp)
-        # dp = [
-        # [1, 4, 5],
-        # [2, 0, 0],
-        # [6, 0, 0]]
+        # 第一行
+        rows_val = 0
+        for ind, val in enumerate(range(n)):
+            rows_val += grid[0][ind]
+            dp[0][ind] = rows_val
+        # 第一列
+        col_val = 0
+        for ind, val in enumerate(range(m)):
+            col_val += grid[ind][0]
+            dp[ind][0] = col_val
+        # 其余的数字
         for i in range(1, m):
             for j in range(1, n):
                 dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
-        pprint(dp)
-        return dp[m - 1][n - 1]
+        return dp[-1][-1]
+
+
 # dp = [
 # [1, 4, 5],
 # [2, 7, 6],
--- unsolved/91 numDecodings.py
+++ unsolved/91 numDecodings.py
@@ -5,34 +5,35 @@ from string import ascii_uppercase
 # 记忆化搜索，递归
 # 动态规划的算法来做，爬楼梯
 class Solution:
-    def __init__(self):
-        self.nums_str = [str(i) for i in range(1, 27)]
-        self.res = []
-
-    def numDecodings(self, s: str) -> int:
-        one_ans = []
-        start = 0
-        self.numDecodings_memo(s, start, one_ans)
-        print(self.res)
-        return len(self.res)
-
-    def numDecodings_memo(self, s, index, one_ans):
-        if index == len(s):
-            self.res.append(one_ans[:])
-            return
-        for i in range(1, len(s)):
-            print(f"{'++  ' * index}for {i=} in {range(1,len(s))= }")
-            print(f'{index = },{i=},{s[index:i] = }')
-            if s[index:i] not in self.nums_str:
-                continue
-            one_ans.append(s[index:i])
-            print(f'{one_ans =}')
-            print(f"----------------------")
-            s = s[index:]
-            self.numDecodings_memo(s, index + 1, one_ans)
-            s = s[index - 1:]
-            one_ans.pop()
-        return
+    # def __init__(self):
+    #     self.nums_str = [str(i) for i in range(1, 27)]
+    #     self.res = []
+    #
+    # def numDecodings(self, s: str) -> int:
+    #     one_ans = []
+    #     start = 0
+    #     self.numDecodings_memo(s, start, one_ans)
+    #     print(self.res)
+    #     return len(self.res)
+    #
+    # def numDecodings_memo(self, s, index, one_ans):
+    #     if index == len(s):
+    #         self.res.append(one_ans[:])
+    #         return
+    #     for i in range(1, len(s)):
+    #         print(f"{'++  ' * index}for {i=} in {range(1,len(s))= }")
+    #         print(f'{index = },{i=},{s[index:i] = }')
+    #         if s[index:i] not in self.nums_str:
+    #             continue
+    #         one_ans.append(s[index:i])
+    #         print(f'{one_ans =}')
+    #         print(f"----------------------")
+    #         s = s[index:]
+    #         self.numDecodings_memo(s, index + 1, one_ans)
+    #         s = s[index - 1:]
+    #         one_ans.pop()
+    #     return
+    pass
 
 
 if __name__ == '__main__':
