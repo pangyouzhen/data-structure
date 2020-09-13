5e9769e19a03c898d8cd5cc12bf1f68e1ccbb5ef
pangyouzhen <pangyouzhen@live.com>
2020-09-13 16:26:42 +0800

    reformat unsolved

--- unsolved/845 longestMountain.py
+++ unsolved/845 longestMountain.py
@@ -1,12 +1,14 @@
+#  动态规划，按顺序递增
+
+#  [1,2,3] 必须是连续的，所以需要记忆上一个山脉尾部的位置
 class Solution(object):
     def longestMountain(self, A):
         """
         :type A: List[int]
         :rtype: int
         """
-        left = 0
-        right = len(A) -1
-        res = 0
-
-        while left < right:
-            s
+        dp = [0] * len(A)
+        dp[0] = 0
+        for i in range(1, len(A)):
+            if A[i] > A[i - 1]:
+                pass
