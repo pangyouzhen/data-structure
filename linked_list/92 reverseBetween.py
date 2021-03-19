fa5d447481e013169ed20c014e171403a76a8c9b
pangyouzhen <pangyouzhen@live.com>
2021-03-19 11:14:03 +0800

    fixed listnode __len__

--- linked_list/92 reverseBetween.py
+++ linked_list/92 reverseBetween.py
@@ -2,12 +2,16 @@ from typing import List
 
 
 class ListNode:
+    """
+    只针对非循环列表
+    """
+
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
     @classmethod
-    def convert_list2listnode(cls, head: List[int]):
+    def list2listnode(cls, head: List[int]):
         head_ = head[::-1]
         curr = None
         listnode = None
@@ -16,18 +20,14 @@ class ListNode:
             curr = listnode
         return listnode
 
-    # TODO
     def __len__(self):
         res = 0
         _ = self
-        while _:
+        #  注意这里判断条件不能是 while _ 否则会陷入无限循环。因为while 会首先判断__bool__属性，随后判定__len__属性，进入无限循环
+        while _ is not None:
             _ = _.next
             res += 1
         return res
-
-    def __iter__(self):
-        if self.next:
-            return self.next
 
 
 class Solution:
@@ -37,20 +37,6 @@ class Solution:
 
 if __name__ == '__main__':
     a = [1, 2, 3, 4, 5]
-    al = ListNode.convert_list2listnode(a)
+    al = ListNode.list2listnode(a)
     print(len(al))
     print("finish")
-    # list_node5 = ListNode(val=5)
-    # list_node4 = ListNode(val=4, next=list_node5)
-    # list_node3 = ListNode(val=3, next=list_node4)
-    # list_node2 = ListNode(val=2, next=list_node3)
-    # list_node = ListNode(val=1, next=list_node2)
-    # # head = [1,2,3,4,5], left = 2, right = 4
-    # # [1,4,3,2,5]
-    # head = [1, 2, 3, 4, 5]
-    # head_ = head[::-1]
-    # curr = None
-    # for i in head_:
-    #     a = ListNode(val=i, next=curr)
-    #     curr = a
-    # print("finish")
