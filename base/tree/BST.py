b1f98f317ec17776896b6061ac58cb8688c5e992
pang <pangyouzhen@live.com>
2021-03-23 20:46:56 +0800

    fixed

--- base/tree/BST.py
+++ base/tree/BST.py
@@ -6,7 +6,7 @@
 #    各个子节点也是如此
 # 特殊性质
 #     二叉查找树的中序遍历是有序的
-from typing import List
+from typing import List, Optional
 
 
 class TreeNode:
@@ -15,39 +15,32 @@ class TreeNode:
         self.left = None
         self.right = None
 
-
-class BST:
-    def __init__(self, nums):
-        self.root = self.sortedArrayToBST(nums)
-
-    # 这个将已经排序的数组转为二叉搜索树，如果没排序的是不可以的
-    # 这个写法和merge sort 写法基本一致
-    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+    @classmethod
+    def from_list2(cls, nums: List[int], sort_bool=True) -> Optional["TreeNode"]:
+        """
+        从列表生成树，sort_bool=True 默认生成二叉搜索树
+        """
+        if sort_bool:
+            nums.sort()
         if not nums:
             return
         mid = len(nums) // 2
         root = TreeNode(nums[mid])
 
-        root.left = self.sortedArrayToBST(nums[:mid])
-        root.right = self.sortedArrayToBST(nums[mid + 1:])
+        root.left = cls.from_list2(nums[:mid])
+        root.right = cls.from_list2(nums[mid + 1:])
         return root
 
-    def __repr__(self):
-        res = []
-        result = self.simpleInorder(self.root, res)
-        return ",".join(result)
-
-    #  二叉搜索树的中序遍历是有序的
-    def simpleInorder(self, root: TreeNode, res: List) -> List:
+    #  中序遍历，只有二叉搜索树的中序遍历是有序的
+    # TODO
+    def simpleInorderBST(self, root: "TreeNode") -> List:
         if root:
-            self.simpleInorder(root.left, res)
-            res.append(str(root.val))
-            self.simpleInorder(root.right, res)
-        return res
+            self.simpleInorderBST(root.left)
+            yield str(root.val)
+            self.simpleInorderBST(root.right)
 
 
 if __name__ == '__main__':
     nums = [3, 2, 1, 4, 8, 5]
-    nums.sort()
-    sol = BST(nums)
-    print(sol)
+    sol = TreeNode.from_list2(nums)
+    print("finish")
---
+++ other/341 NestedIterator.py
@@ -0,0 +1,86 @@
+class NestedInteger:
+
+    def __init__(self, item):
+        super().__init__()
+        if type(item) == list:
+            self.item = [NestedInteger(i) for i in item]
+            self._is_int = False
+        else:
+            self.item = item
+            self._is_int = True
+
+    def isInteger(self) -> bool:
+        """
+        @return True if this NestedInteger holds a single integer, rather than a nested list.
+        """
+        return self._is_int
+
+    def getInteger(self) -> int:
+        """
+        @return the single integer that this NestedInteger holds, if it holds a single integer
+        Return None if this NestedInteger holds a nested list
+        """
+        if self.isInteger():
+            return self.item
+
+    def getList(self) -> ["NestedInteger"]:
+        """
+        @return the nested list that this NestedInteger holds, if it holds a nested list
+        Return None if this NestedInteger holds a single integer
+        """
+        if not self.isInteger():
+            return self.item
+
+
+from collections.abc import Iterable
+
+
+def flatten(items):
+    for x in items:
+        if isinstance(x, Iterable):
+            yield from flatten(x)
+        else:
+            yield x
+
+
+def flatten_list(items, res):
+    for x in items:
+        if isinstance(x, Iterable):
+            flatten_list(x, res)
+        else:
+            res.append(x)
+
+
+items = [1, 2, 3, [4, 5, [6]]]
+# for i in flatten(items):
+#     print(i)
+
+res = []
+flatten_list(items, res)
+print(res)
+
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.nestedList = nestedList
+        self.g = self.gen()
+
+    def gen(self):
+        while self.nestedList:
+            if self.nestedList[0].isInteger():
+                nni = self.nestedList.pop(0)
+                yield nni.getInteger()
+            else:
+                ni = NestedIterator(self.nestedList.pop(0).getList())
+                for c in ni.g:
+                    yield c
+
+    def next(self) -> int:
+        return self.ret
+
+    def hasNext(self) -> bool:
+        try:
+            self.ret = next(self.g)
+            return True
+        except StopIteration:
+            return False
