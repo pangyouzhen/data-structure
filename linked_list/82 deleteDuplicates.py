6768e5ddaf232847612d05f5851bc1f77cdcd8df
pang <pangyouzhen@live.com>
2021-03-25 21:25:42 +0800

    82

--- linked_list/82 deleteDuplicates.py
+++ linked_list/82 deleteDuplicates.py
@@ -1,9 +1,18 @@
 from base.linked_list.ListNode import ListNode
 
 
-class Solution:
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
-        pass
+class Solution(object):
+    def deleteDuplicates(self, head):
+        if not head or not head.next:
+            return head
+        if head.val != head.next.val:
+            head.next = self.deleteDuplicates(head.next)
+        else:
+            move = head.next
+            while move and head.val == move.val:
+                move = move.next
+            return self.deleteDuplicates(move)
+        return head
 
 if __name__ == '__main__':
     head1 = [1, 2, 3, 3, 4, 4, 5]
