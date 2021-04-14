e81b25791cf291e6c14d27b519d4eb67a5cd7492
pangyouzhen <pangyouzhen@live.com>
2021-04-14 11:11:14 +0800

    fixed

--- dynamic programming/slide_window/1052 maxSatisfied.py
+++ dynamic programming/slide_window/1052 maxSatisfied.py
@@ -1,14 +1,18 @@
 from typing import List
 
+
 # todo
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], X: int):
+        if X == len(customers) == len(grumpy):
+            return sum(customers)
         pass
+
 
 
 if __name__ == '__main__':
     sol = Solution()
     customers = [1, 0, 1, 2, 1, 1, 7, 5]
-    grumpy = [0, 1, 0, 1, 0, 1, 0, 1]
+    grumpy =    [0, 1, 0, 1, 0, 1, 0, 1]
     X = 3
-    sol.maxSatisfied(customers, grumpy, X)
+    print(sol.maxSatisfied(customers, grumpy, X))
