8737bcd81f9761f7f901b86ad30a0f185c426d30
pang <pangyouzhen@live.com>
2021-04-18 09:12:33 +0800

    fixed 26

--- array/26 removeDuplicates.py
+++ array/26 removeDuplicates.py
@@ -1,20 +1,21 @@
 class Solution:
     def removeDuplicates(self, nums):
-        a = []
-        i = 0
-        while (i < len(nums)):
-            if nums[i] in a:
-                nums.pop(i)
-                i = i - 1
+        alpha = None
+        alpha_num = 0
+        start = 0
+        while start < len(nums):
+            if nums[start] != alpha:
+                alpha = nums[start]
+                alpha_num += 1
             else:
-                a.append(nums[i])
-            i = i + 1
-        return len(set(nums))
+                del nums[start]
+                start -= 1
+            start += 1
+        return len(nums)
 
 
 if __name__ == '__main__':
     sol = Solution()
     nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-    len_nums = sol.removeDuplicates(nums)
-    for i in range(len_nums):
-        print(nums[i])
+    print(sol.removeDuplicates(nums))
+    print(nums)
