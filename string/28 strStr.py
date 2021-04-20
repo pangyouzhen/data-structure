7119d3bd3a7acc1fec9fa18e9746c7297f79f3ae
pangyouzhen <pangyouzhen@live.com>
2021-04-20 15:47:03 +0800

    fixed 28

--- string/28 strStr.py
+++ string/28 strStr.py
@@ -2,15 +2,13 @@ class Solution:
     def strStr(self, haystack, needle):
         if len(needle) == 0:
             return 0
-        elif len(haystack.split(needle)) > 1:
-            if haystack.split(needle)[0] == "":
-                return 0
-            return len(haystack.split(needle)[0])
-        else:
+        try:
+            return haystack.index(needle)
+        except ValueError:
             return -1
 
 
 if __name__ == '__main__':
     sol = Solution()
-    res = sol.strStr("mississippi", "issip")
+    res = sol.strStr("mississippi", "deissip")
     print(res)
