aacdec126b4c1eb4e6c1b223e6bd7e74002e75b0
pangyouzhen <pangyouzhen@live.com>
2021-05-20 19:20:36 +0800

    fixed

--- slide_window/1052 maxSatisfied.py
+++ slide_window/1052 maxSatisfied.py
@@ -1,18 +1,41 @@
 from typing import List
+import pysnooper
 
 
 # todo
 class Solution:
-    def maxSatisfied(self, customers: List[int], grumpy: List[int], X: int):
-        if X == len(customers) == len(grumpy):
+    @pysnooper.snoop()
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int):
+        if minutes == len(customers) == len(grumpy):
             return sum(customers)
-        pass
-
+        right = 0
+        window = []
+        window2 = []
+        res = 0
+        all_sum = 0
+        for i, v in zip(customers, grumpy):
+            all_sum += i * v
+        while right < len(grumpy):
+            window.append(grumpy[right])
+            window2.append(customers[right])
+            right += 1
+            if len(window) >= minutes:
+                temp = 0
+                for i, v in zip(window, window2):
+                    if i == 0:
+                        temp += v
+                        val = all_sum + temp
+                        if val > res:
+                            res = val
+        return res
 
 
 if __name__ == '__main__':
     sol = Solution()
     customers = [1, 0, 1, 2, 1, 1, 7, 5]
-    grumpy =    [0, 1, 0, 1, 0, 1, 0, 1]
+    grumpy = [0, 1, 0, 1, 0, 1, 0, 1]
     X = 3
+    # customers = [5, 8]
+    # grumpy = [0, 1]
+    # X = 1
     print(sol.maxSatisfied(customers, grumpy, X))
