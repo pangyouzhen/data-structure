f51f0624180f0f1263994597531a3c00b67d649c
pang <pangyouzhen@live.com>
2021-06-26 16:02:30 +0800

    1115

---
+++ other/1115 FooBar.py
@@ -0,0 +1,25 @@
+import threading
+
+empty = threading.Semaphore(1)
+full = threading.Semaphore(0)
+
+
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+
+        for i in range(self.n):
+            empty.acquire()
+            # printFoo() outputs "foo". Do not change or remove this line.
+            printFoo()
+            full.release()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+
+        for i in range(self.n):
+            full.acquire()
+            # printBar() outputs "bar". Do not change or remove this line.
+            printBar()
+            empty.release()
