494c561354525f50e1bbdee9f73766baee2ae272
pang <pangyouzhen@live.com>
2021-07-04 18:49:56 +0800

    union_diff_inter

--- base/dsa/python_data.py
+++ base/dsa/python_data.py
@@ -1,5 +1,3 @@
-
-
 # 2) dict
 
 # 3)  lru_cache
@@ -10,8 +8,21 @@ from functools import lru_cache
 def f():
     pass
 
+
 # 4）list
 # 注意使用zip和enumetre 方法
 
 # 5）tree
 # python 没有内置的树方法，需要自己定义
+
+
+#  python 求交并差集 使用集合进行判断
+
+a = {"a1", "a2", "b1"}
+b = {"b1", "b2", "a1"}
+
+# 交集
+print(a & b)
+print(a - b)
+print(b - a)
+print(a | b)
