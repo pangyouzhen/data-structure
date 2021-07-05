c7e2b12545eba042cfa8dd1349954c8702c9a7a0
pang <pangyouzhen@live.com>
2021-07-05 21:36:47 +0800

    fixed

---
+++ other/726 countOfAtoms.py
@@ -0,0 +1,18 @@
+import re
+import pysnooper
+
+
+# todo
+class Solution:
+    @pysnooper.snoop()
+    def countOfAtoms(self, formula: str) -> str:
+        if "(" not in formula:
+            return
+        new_formula = re.search("\((.*)\)", formula).group(1)
+        self.countOfAtoms(new_formula)
+
+
+if __name__ == '__main__':
+    a = "K4(ON(SO3)2)2"
+    sol = Solution()
+    print(sol.countOfAtoms(a))
