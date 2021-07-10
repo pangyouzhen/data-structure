5c35594d9a56cbf1c1aedeb394bfe6e7f0901988
pang <pangyouzhen@live.com>
2021-07-10 19:40:49 +0800

    fixed

--- other/981 TimeMap.py
+++ other/981 TimeMap.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from bisect import bisect
 import pysnooper
 
-# todo
+
 class TimeMap:
 
     def __init__(self):
@@ -14,7 +14,7 @@ class TimeMap:
     def set(self, key: str, value: str, timestamp: int) -> None:
         self.d[key].append((timestamp, value))
 
-    @pysnooper.snoop()
+    # @pysnooper.snoop()
     def get(self, key: str, timestamp: int) -> str:
         val = self.d.get(key)
         val_dict = dict(val)
@@ -24,14 +24,24 @@ class TimeMap:
             return val_dict[timestamp]
         else:
             ind = bisect(a, timestamp)
+            if ind == 0:
+                return ""
             return val_dict[a[ind - 1]]
 
 
 if __name__ == '__main__':
+    # obj = TimeMap()
+    # obj.set("foo", "bar", 1)
+    # param_2 = obj.get("foo", 1)
+    # print(obj.get("foo", 3))  # 输出 "bar" 因为在时间戳 3 和时间戳 2 处没有对应 "foo" 的值，所以唯一的值位于时间戳 1 处（即 "bar"）
+    # obj.set("foo", "bar2", 4)
+    # print(obj.get("foo", 4))  # 输出 "bar2"
+    # print(obj.get("foo", 5))  # 输出 "bar2"
+    a = ["TimeMap", "set", "set", "get", "get", "get", "get", "get"]
+    b = [[], ["love", "high", 10], ["love", "low", 20], ["love", 5], ["love", 10], ["love", 15], ["love", 20],
+         ["love", 25]]
+
     obj = TimeMap()
-    obj.set("foo", "bar", 1)
-    param_2 = obj.get("foo", 1)
-    print(obj.get("foo", 3))  # 输出 "bar" 因为在时间戳 3 和时间戳 2 处没有对应 "foo" 的值，所以唯一的值位于时间戳 1 处（即 "bar"）
-    obj.set("foo", "bar2", 4)
-    print(obj.get("foo", 4))  # 输出 "bar2"
-    print(obj.get("foo", 5))  # 输出 "bar2"
+    obj.set("love", "high", 10)
+    obj.set("love", "low", 20)
+    print(obj.get("love", 5))
