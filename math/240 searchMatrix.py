9c8c64e1db03b8831cf6d6bd447e894d8ab05186
pang <pangyouzhen@live.com>
2021-10-25 08:09:03 +0800

    fixed

--- math/240 searchMatrix.py
+++ math/240 searchMatrix.py
@@ -3,6 +3,7 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # 时间复杂度 O(m * n)
         for row in matrix:
             for val in row:
                 if val == target:
