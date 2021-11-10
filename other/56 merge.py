c56a1f72325d49f54206c89a99e7438a4380a9f7
pang <pangyouzhen@live.com>
2021-11-10 19:22:00 +0800

    fixed_56

--- other/56 merge.py
+++ other/56 merge.py
@@ -4,19 +4,18 @@ from pysnooper import snoop
 
 class Solution:
     # @snoop()
-    # todo
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        res = []
+        intervals = sorted(intervals, key=lambda x: x[0])
+        res = [intervals[0]]
         pre_min = intervals[0][0]
         pre_max = intervals[0][1]
-        if len(intervals) == 1:
-            res.append([pre_min, pre_max])
         for i in intervals[1:]:
-            if i[0] <= pre_max:
-                res.append([pre_min, i[1]])
-                pre_max = i[1]
-            else:
+            if pre_min <= i[0] <= pre_max:
+                res.pop()
+                pre_min = min(pre_min, i[0])
+                pre_max = max(pre_max, i[1])
                 res.append([pre_min, pre_max])
+            else:
                 res.append([i[0], i[1]])
                 pre_min = i[0]
                 pre_max = i[1]
@@ -26,4 +25,7 @@ class Solution:
 if __name__ == '__main__':
     sol = Solution().merge
     print(sol([[1, 3], [2, 6], [8, 10], [15, 18]]))
-    # print(sol([[1, 4], [5, 6]]))
+    print(sol([[1, 4], [4, 5]]))
+    print(sol([[1, 4], [0, 4]]))
+    print(sol([[1, 5], [2, 3]]))
+    print(sol([[1, 4], [0, 0]]))
