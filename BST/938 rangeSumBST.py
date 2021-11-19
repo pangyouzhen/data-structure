7b118dba98065398d682196d87fc2a817e521d89
pang <pangyouzhen@live.com>
2021-11-19 17:29:29 +0800

    fixed

--- BST/938 rangeSumBST.py
+++ BST/938 rangeSumBST.py
@@ -3,16 +3,16 @@ from base.tree.tree_node import TreeNode
 
 
 class Solution:
+    def __init__(self):
+        self.a = []
+
     def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
-        global a
-        a = []
         self.rangeSumBST_me(root, L, R)
-        return sum(a)
+        return sum(self.a)
 
     def rangeSumBST_me(self, root, L, R):
         if root:
             if L <= root.val <= R:
-                a.append(root.val)
+                self.a.append(root.val)
             self.rangeSumBST_me(root.left, L, R)
             self.rangeSumBST_me(root.right, L, R)
-
