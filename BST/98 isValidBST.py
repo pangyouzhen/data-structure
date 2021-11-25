ba907de2ebbdcce82408da09e76e7645b6847974
pang <pangyouzhen@live.com>
2021-11-25 21:16:03 +0800

    98

--- BST/98 isValidBST.py
+++ BST/98 isValidBST.py
@@ -1,23 +1,21 @@
 # Definition for a binary tree node.
 from base.tree.tree_node import TreeNode
 
-# todo
+
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         # 注意直接递归是不行的，因为可能出现right.left < root 的情况，所以需要另起一个函数记录当前的min和max
         return self.isBst(root, float("-inf"), float("inf"))
 
-    def isBst(self, root: TreeNode, left_value: float, right_value: float):
+    def isBst(self, root: TreeNode, left_value: float, right_value: float) -> bool:
         if root is None:
             return True
-        if root.left is not None and root.right is not None:
-            return root.left.val < root.val < root.right.val
-        if root.left is None and root.right is not None:
-            return root.val < root.right.val
-        if root.left is not None and root.right is None:
-            return root.left.val < root.val
-        left_bool = self.isBst(root.left, root.val, float("inf"))
-        right_bool = self.isBst(root.right, root.val, float("-inf"))
+        if root.val <= left_value or root.val >= right_value:
+            return False
+        # 对于左子树而讲，所有的节点应该都小于root节点
+        left_bool = self.isBst(root.left, left_value, root.val)
+        # 对于右子树而讲，所有的节点都应该大于root节点
+        right_bool = self.isBst(root.right, root.val, right_value)
         return left_bool & right_bool
 
 
@@ -34,4 +32,4 @@ if __name__ == '__main__':
     tree3 = "[5,4,6,null,null,3,7]"
     tree3 = TreeNode.from_strs(tree3)
     print(tree3)
-    assert func(tree1) is False
+    assert func(tree3) is False
