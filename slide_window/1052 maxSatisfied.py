e9e0cf2e166e8fdc996894fe6fc5fedf6de53e0e
pang <pangyouzhen@live.com>
2021-12-11 23:20:51 +0800

    need_prompt

--- slide_window/1052 maxSatisfied.py
+++ slide_window/1052 maxSatisfied.py
@@ -2,22 +2,16 @@ from typing import List
 
 
 class Solution:
-    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int):
-        l = len(customers)
-        val = float("-inf")
-        res = 0
-        for i, j in zip(customers, grumpy):
-            if j == 0:
-                res += i
-        for i in range(l - minutes + 1):
-            temp = 0
-            for c, g in zip(customers[i:i + minutes], grumpy[i:i + minutes]):
-                # 1 是生气
-                if g == 1:
-                    temp += c
-            if temp > val:
-                val = temp
-        return res + val
+    # 思路是对的，但是没剪枝，主要是双list没剪枝
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        n = len(customers)
+        total = sum(c for c, g in zip(customers, grumpy) if g == 0)
+        maxIncrease = increase = sum(c * g for c, g in zip(customers[:minutes], grumpy[:minutes]))
+        for i in range(minutes, n):
+            increase += customers[i] * grumpy[i] - customers[i - minutes] * grumpy[i - minutes]
+            maxIncrease = max(maxIncrease, increase)
+        return total + maxIncrease
+
 
 
 if __name__ == '__main__':
