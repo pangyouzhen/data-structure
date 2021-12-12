4baefd0046603d8649cf3a7034a7f720a7e6130c
pang <pangyouzhen@live.com>
2021-12-12 16:11:17 +0800

    fixed_simcse

--- contest/minimumRefill.py
+++ contest/minimumRefill.py
@@ -1,6 +1,6 @@
 from typing import List
 
-
+# todo
 class Solution:
     def minimumRefill(self, plants: List[int], capacityA: int, capacityB: int) -> int:
         pass
--- contest/subArrayRanges.py
+++ contest/subArrayRanges.py
@@ -5,12 +5,14 @@ class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
         res = 0
         l = len(nums)
-        for i in range(l + 1):
-            for j in range(i + 1, l + 1):
-                n = nums[i:j]
-                max_val = max(n)
-                min_val = min(n)
-                res += (max_val - min_val)
+        for i in range(l):
+            mi = nums[i]
+            ma = nums[i]
+            for j in range(i, l):
+                mi = min(mi, nums[j])
+                ma = max(ma, nums[j])
+                res += (ma - mi)  # 更新差值
+
         return res
 
 
