a0fdf30e3e360ef5af5fc114ec1aa5215e0943a6
pang <pangyouzhen@live.com>
2021-12-16 15:17:59 +0800

    1577

--- other/1577 numTriplets.py
+++ other/1577 numTriplets.py
@@ -1,33 +1,27 @@
+import collections
 from typing import List
-from collections import defaultdict
 
 
 class Solution:
-    def numTriplets(self, nums1: List[int], nums2: List[int]) -> int:
-        global res
-        res = [0]
-        nums1.sort()
-        nums2.sort()
-        self.count_(nums1, nums2)
-        # print(res[0])
-        self.count_(nums2, nums1)
-        return res[0]
+    def __init__(self):
+        self.res = 0
 
-    def count_(self, nums1, nums2):
-        global res
+    def numTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        self.count_(nums1, nums2)
+        self.count_(nums2, nums1)
+        return self.res
+
+    def count_(self, nums1: List[int], nums2: List[int]) -> None:
+        l2 = len(nums2)
         nums1_square = [i ** 2 for i in nums1]
-        m = defaultdict(int)
-        for i, v_val in enumerate(nums2):
-            for j, j_val in enumerate(nums2[i + 1:]):
-                val = v_val * j_val
-                if val in m:
-                    m[val] += 1
-                else:
-                    m[val] = 1
-        # print(m)
+        m = collections.defaultdict(int)
+        for j in range(l2):
+            for i in range(j + 1, l2):
+                val = nums2[i] * nums2[j]
+                m[val] += 1
         for i in nums1_square:
             if i in m.keys():
-                res[-1] += m[i]
+                self.res += m[i]
 
 
 if __name__ == '__main__':
