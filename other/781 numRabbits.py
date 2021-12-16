4b4ef2231ebc651b56f1cb08d44bc4d6352c51e2
pang <pangyouzhen@live.com>
2021-12-16 10:29:31 +0800

    781

--- other/781 numRabbits.py
+++ other/781 numRabbits.py
@@ -1,14 +1,24 @@
+import math
 from typing import List
 from collections import Counter
 
 
 class Solution:
-    # todo
     def numRabbits(self, answers: List[int]):
-        pass
+        res = 0
+        c = Counter(answers)
+        for k, v in c.items():
+            if k <= v:
+                color_number = math.ceil(v / (k + 1))
+                res += color_number * (k + 1)
+            else:
+                res += k + 1
+        return res
 
 
 if __name__ == '__main__':
-    answers = [1, 0, 1, 0, 0]
+    # answers = [1, 0, 1, 0, 0]
+    # answers = [10, 10, 10]
+    answers = [1, 1, 2]
     sol = Solution()
     print(sol.numRabbits(answers))
