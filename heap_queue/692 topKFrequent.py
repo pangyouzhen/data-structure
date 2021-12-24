e4fdf4b38b295ad9b1ace32b148e9bd10d1f0c1b
pang <pangyouzhen@live.com>
2021-12-24 15:26:58 +0800

    heap

--- heap_queue/692 topKFrequent.py
+++ heap_queue/692 topKFrequent.py
@@ -1,20 +1,40 @@
 from typing import List
 import heapq
-import pysnooper
+from collections import Counter
+
+
+class Word:
+    def __init__(self, word, fre) -> None:
+        self.word = word
+        self.fre = fre
+
+    def __lt__(self, other):
+        if self.fre != other.fre:
+            return self.fre < other.fre
+
+        return self.word > other.word
 
 
 class Solution:
-    @pysnooper.snoop()
-    # todo
     # 这里的不一样是在 如果相同的按照首字母的拼写来取
+
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        pass
+        counter = Counter(words)
+        heaps = [] 
+        for word,fre in counter.items():
+            heapq.heappush(heaps,Word(word,fre))
+            # 维护一个长度为k的优先队列
+            if len(heaps) > k:
+                heapq.heappop(heaps)
+        heaps.sort(reverse=True)
+        return [x.word for x in heaps]
 
 
 if __name__ == '__main__':
     sol = Solution()
-    # words = ["i", "love", "leetcode", "i", "love", "coding"]
-    # k = 3
-    words = ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"]
-    k = 4
+    words = ["i", "love", "leetcode", "i", "love", "coding"]
+    k = 3
+    # words = ["the", "day", "is", "sunny", "the",
+    #  "the", "the", "sunny", "is", "is"]
+    # k = 4
     print(sol.topKFrequent(words, k))
