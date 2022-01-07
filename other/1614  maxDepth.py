15fe8c286577240d9896ccfb8d240c9ee25c09ba
pang <pangyouzhen@live.com>
2022-01-07 16:13:10 +0800

    1614

--- other/1614  maxDepth.py
+++ other/1614  maxDepth.py
@@ -1,22 +1,17 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        if len(s) == 1:
-            return 0
-        res = [False] * (len(s))
-        if s[0] == "(":
-            res[0] = 1
-        else:
-            res[0] = 0
-        for i in range(1, len(s)):
-            if s[i] == "(":
-                res[i] = res[i - 1] + 1
-            elif s[i] == ")":
-                res[i] = res[i - 1] - 1
-            else:
-                res[i] = res[i - 1]
-        return max(res)
+        m = 0
+        res = 0
+        for i, v in enumerate(s):
+            if v == "(":
+                res += 1
+                if res > m:
+                    m = res
+            elif v == ")":
+                res -= 1
+        return m
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.maxDepth("(1)+((2))+(((3)))"))
+    print(sol.maxDepth("1"))
