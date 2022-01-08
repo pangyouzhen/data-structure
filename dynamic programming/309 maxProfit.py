d082b22da3f92aaddbaa37274e0b4c0ed0b96cf5
pang <pangyouzhen@live.com>
2022-01-08 16:42:34 +0800

    309

--- dynamic programming/309 maxProfit.py
+++ dynamic programming/309 maxProfit.py
@@ -1,10 +1,22 @@
 from typing import List
 
 
-# TODO
 class Solution:
+    # todo
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        l = len(prices)
+        if l == 1:
+            return 0
+        elif l  == 2:
+            return max(prices[1] - prices[0], 0)
+        elif l == 3:
+            return max(prices[2] - prices[1], prices[2]-prices[0], prices[1] - prices[0],0) 
+        dp = [0] * l
+        dp[1] = max(prices[1] - prices[0], 0)
+        dp[2] = max(prices[2] - prices[1], prices[2]-prices[0], dp[1])
+        for i in range(3, l):
+            dp[i] = max(dp[i-1], dp[i-3]+prices[i]-prices[i-1])
+        return dp[-1]
 
 
 if __name__ == '__main__':
