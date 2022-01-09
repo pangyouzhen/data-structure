d47aebd3e43fb45f41b08c14b8c27f0b3f0128cc
pang <pangyouzhen@live.com>
2022-01-09 15:50:28 +0800

    2016

---
+++ base/dp/2016 maximumDifference.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution():
+    def maximumDifference(self, nums: List[int]) -> int:
+        l = len(nums)
+        max_val = -1
+        for i in range(l):
+            for j in range(i+1,l):
+                print(i,j)
+                if nums[j] - nums[i] > 0:
+                     max_val = max(nums[j] - nums[i],max_val)
+        return max_val
+
+if __name__ =="__main__":
+    func = Solution().maximumDifference
+    nums =[7,1,5,4]
+    print(func(nums))
