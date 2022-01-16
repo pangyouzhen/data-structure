029708962e4330bc1e0eba40c03285daee44b08d
pang <pangyouzhen@live.com>
2022-01-16 14:26:57 +0800

    fixed

--- contest/mostPoints.py
+++ contest/mostPoints.py
@@ -1,22 +1,18 @@
 from typing import List
-from collections import defaultdict
+
+from pysnooper import snoop
 
 
 class Solution:
-    # todo
+    # @snoop()
     def mostPoints(self, questions: List[List[int]]) -> int:
-        a = defaultdict(int)
-        l = len(questions)
-        dp = [0] * l
-        dp[0] = questions[0][0]
-        for i, v in enumerate(questions):
-            a[i] = i + questions[i][1]
-            reverse_dic = {val: key for key, val in a.items()}
-            if i == 0:
-                dp[i] = max(dp[i - 1],questions[i][0])
-            else:
-                dp[i] = dp[reverse_dic[i]] + questions[i][0]
-        return dp[-1]
+        n = len(questions)
+        f = [0] * (n + 1)
+        for i in range(n-1, -1, -1):
+            q = questions[i]
+            j = i + q[1] + 1
+            f[i] = max(f[i+1], q[0]+(f[j] if j < n else 0))
+        return f[0]
 
 
 if __name__ == '__main__':
