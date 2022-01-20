736edd6a43f7b8ec02cbc3006fc8aca6cd0481bf
pang <pangyouzhen@live.com>
2022-01-20 19:52:36 +0800

    offer15

---
+++ other/1220 countVowelPermutation.py
@@ -0,0 +1,11 @@
+from typing import List
+
+# todo
+class Solution():
+    def countVowelPermutation(self, n: int) -> int:
+        pass
+
+if __name__ =="__main__":
+    func = Solution().countVowelPermutation
+    n = 5
+    print(func(n))
