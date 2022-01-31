635c02dcc9ba02f8647a2719611cfc69bd4a38d0
pang <pangyouzhen@live.com>
2022-01-31 10:04:21 +0800

    fixed

--- string_/1342 numberOfSteps.py
+++ string_/1342 numberOfSteps.py
@@ -1,18 +1,13 @@
-from pysnooper import snoop
-
 
 class Solution:
-    # @snoop()
     def numberOfSteps(self, num: int) -> int:
         res = 0
         while num != 0:
             if num % 2 == 1:
                 num = num - 1
-                res += 1
-                continue
             else:
                 num = num / 2
-                res += 1
+            res += 1
         return res
 
 
