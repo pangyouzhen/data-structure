3498e2b70ab86385064d00c13877a2b1227906c7
pang <pangyouzhen@live.com>
2022-02-20 19:43:48 +0800

    617

--- base/tree/617 mergeTrees.py
+++ base/tree/617 mergeTrees.py
@@ -4,23 +4,23 @@ from base.tree.tree_node import TreeNode
 
 
 class Solution:
-    # TODO
-    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
-        """
-        合并好左子树
-        """
-        # if root1 is not None and root2 is not None:
-        #     return TreeNode(root1.val + root2.val)
-        # elif root1 is None and root2 is not None:
-        #     return TreeNode(root2.val)
-        # elif root1 is not None and root2 is None:
-        #     return TreeNode(root1.val)
-        # left_tree = self.mergeTrees(root1.left, root2.left)
-        # right_tree = self.mergeTrees(root1.right, root2.right)
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        # 只关注函数的宏观语义就好，不然容易陷入进去
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+
+        merged = TreeNode(t1.val + t2.val)
+        merged.left = self.mergeTrees(t1.left, t2.left)
+        merged.right = self.mergeTrees(t1.right, t2.right)
+        return merged
 
 
 if __name__ == '__main__':
-    root1 = TreeNode()
-    root2 = TreeNode()
+    root1 = TreeNode.from_strs("[1,3,2,5]")
+    root2 = TreeNode.from_strs("[2,1,3,null,4,null,7]")
+    print(root1)
+    print(root2)
     sol = Solution()
     print(sol.mergeTrees(root1, root2))
