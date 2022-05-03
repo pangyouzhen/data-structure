9da5d79cc5d5ff292432dcd3bd508736e348b162
pang <pangyouzhen@live.com>
2022-05-03 15:47:32 +0800

    levelorder

--- tree/102 levelOrder.py
+++ tree/102 levelOrder.py
@@ -1,28 +1,42 @@
 from base.tree.tree_node import TreeNode
-
+from typing import List
 
 class Solution:
-    def levelOrder(self, root: TreeNode):
-        if root is None:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
             return []
-        result, queue = [], [root]
-        while queue:
-            next_level, curr_vals = [], []
-            for node in queue:
-                if node.val is not None:
-                    curr_vals.append(node.val)
-                if node.left:
-                    next_level.append(node.left)
-                if node.right:
-                    next_level.append(node.right)
-            queue = next_level
-            result.append(curr_vals)
-        return result
+        res,curr = [],[root]
+        while curr:
+            next_levels,curr_vals = [],[]
+            for i in curr:
+                if i.left:
+                    next_levels.append(i.left)
+                if i.right:
+                    next_levels.append(i.right)
+                curr_vals.append(i.val)
+            curr = next_levels
+            res.append(curr_vals)
+        return res
+        
+                
 
+    def level2(self,root:TreeNode) -> List[int]:
+        res, curr = [],[root]
+        while curr:
+            node = curr.pop(0)
+            if node.left:
+                curr.append(node.left)
+            if node.right:
+                curr.append(node.right)
+            res.append(node.val)
+        return res
+    
+    
 
 if __name__ == '__main__':
     tree = TreeNode.from_strs("[3, 9, 20, null, null, 15, 7]")
     print(tree)
     sol = Solution()
     t = sol.levelOrder(tree)
+    # t = sol.level2(tree)
     print(t)
