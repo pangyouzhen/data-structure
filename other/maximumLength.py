816d07bf9d24c00810d087084845334d86b40c80
pang <pangyouzhen@live.com>
2024-01-28 15:58:30 +0800

    fixed

--- other/maximumLength.py
+++ other/maximumLength.py
@@ -9,7 +9,7 @@ class Solution:
         n = c[1]
         # 偶数要取到奇数
         if n % 2 ==0:
-            curr = max((n/2) - 1,curr)
+            curr = max(n - 1,curr)
         else:
             curr = n
         del c[1]
@@ -20,7 +20,7 @@ class Solution:
                     # 思路对了,写起来错了
                     res -= 1
                     break
-                if c[i] % 2== 1:
+                if c[i] == 1:
                     res += 1
                     break
                 res += 2
@@ -32,9 +32,11 @@ class Solution:
 # nums0 = [5, 4, 1, 2, 2]
 # nums1 = [1, 3, 2, 4]
 # nums2 = [1, 3, 2, 4, 9, 3, 3]
-nums3 = [1, 1, 1]
+# nums3 = [1, 1, 1]
+# nums = [4,36,9,16,1,1,4,121,64,4]
+nums = [1,1,1,1,1,1,1,1,1,1,2,4,8,16,32,64,128,256,512,1024]
 func = Solution().maximumLength
 # print(func(nums0))
 # print(func(nums1))
 # print(func(nums2))
-print(func(nums3))
+print(func(nums))
