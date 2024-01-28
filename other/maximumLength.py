824606ad8c02c3e515435162293d146009591bed
pang <pangyouzhen@live.com>
2024-01-28 15:52:17 +0800

    fixed

--- other/maximumLength.py
+++ other/maximumLength.py
@@ -5,43 +5,28 @@ from typing import List
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
         c = Counter(nums)
-        ks = list(c.keys())
-        ks.sort()
-        res = 1
-        for i in ks:
-            curr = 1
-            if i == 1:
-                n = c[i]
-                if n % 2 == 0:
-                    curr = (n / 2) - 1
-                else:
-                    curr = n
-                    continue
-            two_ = 1
-            # 必须对称的2个元素
-            if c[i] % 2 != 0:
-                continue
-            while two_:
-                v = i ** two_
-                # 如果在这里面
-                if v in ks:
-                    # 只有1个时候
-                    if c[v] == 1:
-                        break
-                    # 如果>=2个
-                    else:
-                        # 也有可能+2, 也可能+1
-                        # +2 是 下一个元素有，
-                        # +1 是：下一个元素没有
-                        # 深度优先算法，直到没有的时候终止
-                        two_ += 1
-                        t = i ** two_
-                        if t in ks:
-                            curr += 2
-                        else:
-                            curr += 1
-            res = max(res, curr)
-        return res
+        curr = 1
+        n = c[1]
+        # 偶数要取到奇数
+        if n % 2 ==0:
+            curr = max((n/2) - 1,curr)
+        else:
+            curr = n
+        del c[1]
+        for i in c:
+            res = 0
+            while True:
+                if i not in c:
+                    # 思路对了,写起来错了
+                    res -= 1
+                    break
+                if c[i] % 2== 1:
+                    res += 1
+                    break
+                res += 2
+                i *= i
+            curr = max(res, curr)
+        return curr
 
 
 # nums0 = [5, 4, 1, 2, 2]
